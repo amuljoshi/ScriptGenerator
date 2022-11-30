037cc48425822cccdc2618b682c5ff7fa4002111
amuljoshi <83940325+amuljoshi@users.noreply.github.com>
2022-11-30 15:30:58 +0545

    Updating the RunMe.bat file

--- RunMe.bat
+++ RunMe.bat
@@ -2,11 +2,11 @@
 title Starting RunMe.bat
 :: See the title at the top
 echo Changing the directory containing Python File...
-cd "C:\Users\amjoshi\OneDrive - Ncell Axiata Limited\Documents\Ex_Files_Learning_Python_Upd\Exercise Files\MyCodes\ScriptGenerator"
+cd "Folder where the python file is present in local drive"
 echo Changed directory successfully
 title Running the Python script file
 echo Running the Python file to generate scripts...
-"C:\Program Files\Python39\python.exe" AAA.py
+"Location of python.exe file" AAA.py
 echo End of script!!
 title End of RunMe.bat
 Pause
